69ed01e641002fd9e03aee9b5ac4c830432fefb1
torshow <slywoods12@gmail.com>
2026-03-24 00:18:49 +0300

    add inventoryManagement.js

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,7 +1,23 @@
 // Write your code here
+let products = ["Laptop", "Phone", "Headphones", "Monitor",];
+function logFirstProduct() {
+  console.log(products[0]);
+  
+}
+function addProduct(product) {
+  products.push(product);
+}
+function updateProductName(index, newName) {
+  if (index >= 0 && index < products.length) {
+    products[index] = newName;
+  } else {
+    console,log("index out of bounds");
 
-
-
+  }
+}
+function removeLastProduct() {
+  products.pop();
+}
 // Export the necessary parts for testing
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
